e64c2f19778cc47f566496ff61c17c7cc5392c7d
sherylynn <352281674@qq.com>
2014-05-15 12:35:11 +0800

    1
    1

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -134,7 +134,7 @@ app.controller('MainController', function($rootScope, $scope, $routeParams, Apoi
   $scope.markdown="#你好";
 
   $scope.search={
-    "临床表现":""
+    "临床表现":"呕吐"
   };
   $scope.search_page={
     test:function(){
